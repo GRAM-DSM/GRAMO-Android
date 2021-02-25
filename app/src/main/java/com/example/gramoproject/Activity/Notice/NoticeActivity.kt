f4df8397c73c9b96b2cb4645924bd652eb26545c
Anbyeonghun <abh0920one@gmail.com>
2021-02-25 20:40:08 +0900

    Notice

--- app/src/main/java/com/example/gramoproject/Activity/Notice/NoticeActivity.kt
+++ app/src/main/java/com/example/gramoproject/Activity/Notice/NoticeActivity.kt
@@ -5,30 +5,37 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.gramo.BottomSheet.NoticeBottomSheetDialogFragment
 import com.example.gramo.R
 import com.example.gramoproject.Activity.Calendar.CalendarActivity
 import com.example.gramoproject.Activity.Homework.HomeworkMainActivity
 import com.example.gramoproject.Activity.SignInUp.LoginActivity
 import com.example.gramoproject.Adapter.NoticeRecyclerAdapter
 import com.example.gramoproject.DataClass.NoticeItem
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.leave_custom_dialog.*
 import kotlinx.android.synthetic.main.logout_custom_dialog.*
 import kotlinx.android.synthetic.main.notice_activity.*
 import kotlinx.android.synthetic.main.notice_appbar.*
+import kotlinx.android.synthetic.main.notice_bottomsheet.*
+import kotlinx.android.synthetic.main.notice_unload_dialog.*
 
 class NoticeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     //현재 액티비티 확인
     private val currentActivity = javaClass.simpleName.trim()
-    //로그아웃 알림창
+    //커스텀 알림창
     private lateinit var LogoutDialog: Dialog
     private lateinit var LeaveDialog: Dialog
+    private lateinit var UnloadDialog: Dialog
+    //마지막으로 뒤로 가기 버튼을 누른 시간 저장
+    private var backKeyPressedTime : Long = 0
+    private lateinit var toast: Toast
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +44,26 @@ class NoticeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelec
         //Navigation Drawer
         NavInitializeLayout()
         notice_nav_view.setNavigationItemSelectedListener(this)
+
+        //로그아웃 dialog 커스텀
+        LogoutDialog = Dialog(this)
+        LogoutDialog.setContentView(R.layout.logout_custom_dialog)
+        //로그아웃 클릭
+        side_logout_btn.setOnClickListener {
+            showLogoutDialog()
+        }
+
+        //탈퇴 dialog 커스텀
+        LeaveDialog = Dialog(this)
+        LeaveDialog.setContentView(R.layout.leave_custom_dialog)
+        //탈퇴 클릭
+        side_leave_btn.setOnClickListener{
+            showLeaveDialog()
+        }
+
+        //공지 내리기 dialog 커스텀
+        UnloadDialog = Dialog(this)
+        UnloadDialog.setContentView(R.layout.notice_unload_dialog)
 
         //notice_add로 이동
         notice_add_btn.setOnClickListener{
@@ -65,28 +92,30 @@ class NoticeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelec
         adapter.setOnItemClickListener(object : NoticeRecyclerAdapter.OnNoticeItemClickListener{
             override fun onItemClick(v: View, data: NoticeItem, position: Int) {
                 //bottom sheet 띄우기
-                val bottomSheetDialogFragment = NoticeBottomSheetDialogFragment()
-                bottomSheetDialogFragment.show(supportFragmentManager, bottomSheetDialogFragment.tag)
+                val bottomSheetDialog = BottomSheetDialog(this@NoticeActivity)
+                bottomSheetDialog.setContentView(R.layout.notice_bottomsheet)
+                bottomSheetDialog.notice_date_et.text = getIntent.getStringExtra("date")
+                bottomSheetDialog.notice_title_tv2.text = getIntent.getStringExtra("title")
+                bottomSheetDialog.notice_contents_tv2.text = getIntent.getStringExtra("contents")
+                bottomSheetDialog.show()
 
+                //공지 내리기
+                bottomSheetDialog.notice_unload_btn.setOnClickListener{
+                    UnloadDialog.show()
+                    //Negative Button
+                    UnloadDialog.unload_negative_btn.setOnClickListener{
+                        UnloadDialog.dismiss()
+                    }
+                    //Positive Button
+                    UnloadDialog.unload_positive_btn.setOnClickListener{
+                        UnloadDialog.dismiss()
+                        bottomSheetDialog.dismiss()
+                        adapter.removeItem(position)
+                        Toast.makeText(applicationContext, "공지사항이 삭제되었습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                }
             }
         })
-
-        //로그아웃 dialog 커스텀
-        LogoutDialog = Dialog(this)
-        LogoutDialog.setContentView(R.layout.logout_custom_dialog)
-        //로그아웃 클릭
-        side_logout_btn.setOnClickListener {
-            showLogoutDialog()
-        }
-
-        //탈퇴 dialog 커스텀
-        LeaveDialog = Dialog(this)
-        LeaveDialog.setContentView(R.layout.leave_custom_dialog)
-        //탈퇴 클릭
-        side_leave_btn.setOnClickListener{
-            showLeaveDialog()
-        }
-
     }
 
     //Navigation Drawer 설정
@@ -147,7 +176,20 @@ class NoticeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelec
         if(drawer_layout.isDrawerOpen(GravityCompat.START)){
             drawer_layout.closeDrawers()
         } else{
-            super.onBackPressed()
+            //super.onBackPressed()
+            //마지막으로 뒤로가기를 누른 후 2.5초가 지났을 경우
+            if (System.currentTimeMillis() > backKeyPressedTime + 2500) { //2500ms = 2.5s
+                backKeyPressedTime = System.currentTimeMillis()
+                toast = Toast.makeText(applicationContext, "뒤로 가기 버튼을 한 번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT)
+                toast.show()
+                return
+            }
+
+            //마지막으로 뒤로가기를 누른 후 2.5초가 지나지 않았을 경우
+            if(System.currentTimeMillis() <= backKeyPressedTime + 2500){ //2500ms = 2.5s
+                finishAffinity()
+                toast.cancel()
+            }
         }
     }
 
--- app/src/main/java/com/example/gramoproject/Activity/Notice/NoticeAddActivity.kt
+++ app/src/main/java/com/example/gramoproject/Activity/Notice/NoticeAddActivity.kt
@@ -1,12 +1,57 @@
 package com.example.gramoproject.Activity.Notice
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gramo.R
+import kotlinx.android.synthetic.main.notice_add_activity.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class NoticeAddActivity : AppCompatActivity() {
+
+    //현재 날짜 저장
+    private val format = SimpleDateFormat("yyyy년 MM월 dd일")
+    private val now = System.currentTimeMillis()
+    private lateinit var date : Date
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.notice_add_activity)
+
+        notice_date_et.setText(getTime())
+
+        //취소 클릭
+        notice_cancel_tv.setOnClickListener{
+            val intentToNotice = Intent(applicationContext, NoticeActivity::class.java)
+            intentToNotice.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
+            startActivity(intentToNotice)
+            finish()
+        }
+
+        //완료 클릭
+        notice_complete_tv.setOnClickListener{
+            //제목, 내용이 비어있을 경우 경고문 표시
+            if(notice_title_et.text.toString().equals("") || notice_content_et.text.toString().equals("")){
+                Toast.makeText(applicationContext, "제목 또는 내용을 입력해주세요", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                val intentToNotice = Intent(applicationContext, NoticeActivity::class.java)
+                intentToNotice.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+                intentToNotice.putExtra("name", notice_name_et.text.toString())
+                intentToNotice.putExtra("date", notice_date_et.text.toString())
+                intentToNotice.putExtra("title", notice_title_et.text.toString())
+                intentToNotice.putExtra("contents", notice_content_et.text.toString())
+                startActivity(intentToNotice)
+                finish()
+            }
+        }
+
+    }
+
+    fun getTime(): String{
+        date = Date(now)
+        return format.format(date)
     }
 }
