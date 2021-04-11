785001414fbdf4e23eb38875e094952c8fd40f5c
Anbyeonghun <abh0920one@gmail.com>
2021-04-11 12:41:16 +0900

    Create a Sign interface

--- app/src/main/java/com/example/gramoproject/Interface/LoginInterface.kt
+++ app/src/main/java/com/example/gramoproject/Interface/LoginInterface.kt
@@ -1,4 +1,22 @@
 package com.example.gramoproject.`interface`
 
+import com.example.gramoproject.dataclass.Login
+import com.example.gramoproject.dataclass.TokenModel
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.http.*
+
 interface LoginInterface {
+    @POST("/auth")
+    fun signIn(
+            @Body login: Login
+    ): Call<TokenModel>
+
+    @DELETE("/auth")
+    fun logout(
+            @Header("Authorization")header: String) : Call<Unit>
+
+    @GET("/auth")
+    fun tokenRefresh(
+            @Header("Authorization")header: String) : Response<TokenModel>
 }
--- app/src/main/java/com/example/gramoproject/Interface/RegisterInterface.kt
+++ app/src/main/java/com/example/gramoproject/Interface/RegisterInterface.kt
@@ -1,4 +1,24 @@
 package com.example.gramoproject.`interface`
 
+import com.example.gramoproject.dataclass.EmailAuth
+import com.example.gramoproject.dataclass.UserModel
+import com.google.gson.JsonObject
+import retrofit2.Call
+import retrofit2.http.*
+
 interface RegisterInterface {
+    @POST("/signup")
+    fun signUp(
+            @Body user : UserModel
+    ): Call<Unit>
+
+    @POST("/sendemail")
+    fun sendEmail(
+            @Body email: JsonObject
+    ) : Call<Unit>
+
+    @POST("/checkcode")
+    fun checkEmailAuthenticationCode(
+            @Body authInfo : EmailAuth
+    ) : Call<Unit>
 }
